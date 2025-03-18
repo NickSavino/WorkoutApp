f9fc232697d3707e303d3de28b7cb1e61cbc15c5
Gabriel C <gabriel.craiovan@ucalgary.ca>
2025-03-17 19:15:39 -0600

    Updated Nav bar UI

--- workoutapp.client/src/components/Navbar.tsx
+++ workoutapp.client/src/components/Navbar.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   const { logout } = useAuth();
+  const navigate = useNavigate();
 
   return (
     <header className="bg-[#26455D] text-white p-4">
-      <div className="flex justify-between items-start">
-        <h2 className="text-2xl font-bold">Jym</h2>
-        <button
-          onClick={logout}
-          className="bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded-md transition"
+      <div className="flex justify-between items-center">
+        <h2
+          className="text-2xl font-bold cursor-pointer"
+          onClick={() => navigate("/home")}
         >
-          Logout
-        </button>
+          Jym
+        </h2>
+        <nav className="flex items-center space-x-2">
+          <button onClick={() => console.log("Profile")}>Profile</button>
+          <button onClick={() => console.log("Workout")}>Workout</button>
+          <button
+            onClick={logout}
+            className="bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded-md transition"
+          >
+            Logout
+          </button>
+        </nav>
       </div>
     </header>
   );
