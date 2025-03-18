45ce7667e715a400fed5138fd18520f532b2cb6e
abdulmoizbhatti <abdulmoiz.bhatti@ucalgary.ca>
2025-03-18 14:37:29 -0600

    implemented the workout page which consists of the different muscle categories, hard code of all exercises in those muscle groups including descriptions. additionally, added a routine section which you can edit, delete and create which involves title generation and selecting any exercise from the previously mentioned muscle groups. needed: proper implementation of user_id connections and everything database related. the workout tsx file currently has sections where this can be connected to

--- workoutapp.client/src/App.tsx
+++ workoutapp.client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import LoginPage from "./components/LoginPage";
+import WorkoutPage from "./components/WorkoutPage";
 import { useAuth } from "./context/AuthContext";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Route path="/" element={<Navigate to="/login" replace />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/home" element={<LandingPage />} />
+            <Route path="/workout" element={<WorkoutPage />} />
             <Route path="*" element={<Navigate to={user ? "/home" : "/login"} replace />} />
         </Routes>
     );
