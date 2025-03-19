211441265fe9db482c95b194767f9651bab99424
AliHas1212 <113304393+AliHas1212@users.noreply.github.com>
2025-03-18 19:43:10 -0600

    Update App.tsx
    added workout

--- workoutapp.client/src/App.tsx
+++ workoutapp.client/src/App.tsx
@@ -3,10 +3,10 @@ import LandingPage from "./components/LandingPage";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import ProfilePage from "./components/ProfilePage";
+import WorkoutPage from "./components/WorkoutPage";
 import { useAuth } from "./context/AuthContext";
 
 function App() {
-
     const { user } = useAuth();
 
     return (
@@ -16,6 +16,7 @@ function App() {
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/home" element={<LandingPage />} />
             <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/workout" element={<WorkoutPage />} />
             <Route path="*" element={<Navigate to={user ? "/home" : "/login"} replace />} />
         </Routes>
     );
