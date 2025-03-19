d7aab6bb0fb7c239e5a17ccb1fda1259c94d075e
Zaid <zaid.ahmed2080@gmail.com>
2025-03-18 19:05:44 -0600

    User model test (#10)
    * added the appsettings to the gitignore so I don't have to modify it every 30 seconds
    
    * installing dependencies for the testing framework
    
    * added test file and testdbcontext
    
    * correct the test project structure by movign tests alongside server and client
    
    * correct namespace and usage issues to get the first two tests to run
    
    * Added Test suite to Visual Studio solution for testing integration
    
    * Renamed the User test file to be consistend and moved setup and teardown logic into a base test class
    
    ---------
    
    Co-authored-by: NickSavino <nicksavino2@gmail.com>

---
+++ WorkoutApp.Tests/TestAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkoutApp.Server;
+using WorkoutApp.Server.Model;
+
+namespace WorkoutApp.Tests
+{
+    public class TestAppDbContext : AppDbContext
+    {
+        public TestAppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Use in-memory database for testing
+            optionsBuilder.UseInMemoryDatabase("TestDatabase");
+        }
+    }
+}
