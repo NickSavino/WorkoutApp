4519e70d1dad581f15e49c1fd8c48c03ca9a3b7c
Zaid <zaid.ahmed@ucalgary.ca>
2025-03-18 23:21:26 -0600

    Made the appropriate Hash changes to the Workout Test class

--- WorkoutApp.Tests/UserTest.cs
+++ WorkoutApp.Tests/UserTest.cs
@@ -9,12 +9,7 @@ namespace WorkoutApp.Tests
     [TestClass]
     public class UserTest : BaseTest
     {
-        private static string HashPassword(string password)
-        {
-            using var sha256 = SHA256.Create();
-            var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-            return Convert.ToBase64String(bytes);
-        }
+
 
         [TestMethod]
         public void AddUser_ShouldIncreaseCount()
