5285bd2b703c506f700326209a8db30d31996c7f
NickSavino <nicksavino2@gmail.com>
2025-03-19 06:14:57 -0600

    Updated Workout and Exercise tables db to handle more complex logic. Sets, reps, weights, and notes now exist in the junction table that connects them, so we have a proper many to many relationship now.

--- WorkoutApp.Server/Migrations/20250319090354_UpdateWorkoutExerciseTable.Designer.cs
+++ WorkoutApp.Server/Migrations/20250319090354_UpdateWorkoutExerciseTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkoutApp.Server;
 
@@ -11,9 +12,11 @@ using WorkoutApp.Server;
 namespace WorkoutApp.Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250319090354_UpdateWorkoutExerciseTable")]
+    partial class UpdateWorkoutExerciseTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
