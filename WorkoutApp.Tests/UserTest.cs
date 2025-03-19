01d497758cda97b36f42d0d4fd05a57826f78b50
Zaid <zaid.ahmed2080@gmail.com>
2025-03-18 23:28:26 -0600

    Test hash bug fix (#14)
    * added HashPassword to the BaseTest class to fix the required bug on the Hashed Password. Not the DRYest thing but whatever
    
    * Made the appropriate Hash changes to the Workout Test class

--- WorkoutApp.Tests/UserTest.cs
+++ WorkoutApp.Tests/UserTest.cs
@@ -1,16 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using WorkoutApp.Server.Model;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace WorkoutApp.Tests
 {
     [TestClass]
     public class UserTest : BaseTest
     {
+
+
         [TestMethod]
         public void AddUser_ShouldIncreaseCount()
         {
-            var user = new User { Name = "Test User", Email = "test@example.com" };
+            var user = new User
+            {
+                Name = "Test User",
+                Email = "test@example.com",
+                PasswordHash = HashPassword("securepassword")
+            };
             _context.User.Add(user);
             _context.SaveChanges();
 
@@ -20,13 +29,19 @@ namespace WorkoutApp.Tests
         [TestMethod]
         public void GetUser_ShouldReturnCorrectUser()
         {
-            var user = new User { Name = "Test User", Email = "test@example.com" };
+            var user = new User
+            {
+                Name = "Test User",
+                Email = "test@example.com",
+                PasswordHash = HashPassword("securepassword")
+            };
             _context.User.Add(user);
             _context.SaveChanges();
 
             var fetchedUser = _context.User.FirstOrDefault(u => u.Email == "test@example.com");
             Assert.IsNotNull(fetchedUser);
             Assert.AreEqual("Test User", fetchedUser.Name);
+            Assert.AreEqual(user.PasswordHash, fetchedUser.PasswordHash);
         }
     }
 }
