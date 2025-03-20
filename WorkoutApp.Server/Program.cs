3a9a7df13d89a9ac033c6b4051f5996e10665897
NickSavino <nicksavino2@gmail.com>
2025-03-20 02:28:13 -0600

    updated program.cs and appsettings.json

--- WorkoutApp.Server/Program.cs
+++ WorkoutApp.Server/Program.cs
@@ -46,7 +46,7 @@ var builder = WebApplication.CreateBuilder(args);
 
     // Register DBContext
     builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("WorkoutApp_Dev")));
+        options.UseSqlServer(builder.Configuration.GetConnectionString("WorkoutAppDev")));
 
     var app = builder.Build();
 
